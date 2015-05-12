401dafc476e4103a22f54998c8889f389d49a6af
Inbound Now <david@inboundnow.com>
2015-05-12 13:25:48 -0700

    remove factory call in browser entry point

--- app/main.js
+++ app/main.js
@@ -1,8 +1,8 @@
     /** @jsx React.DOM */
 
 var React = require('react/addons');
-var ReactApp = React.createFactory(require('./components/ReactApp').ReactApp);
+var ReactApp = require('./components/ReactApp');
 
 var mountNode = document.getElementById("react-main-mount");
 
-React.renderComponent(new ReactApp({}), mountNode);
+React.render(new ReactApp({}), mountNode);
