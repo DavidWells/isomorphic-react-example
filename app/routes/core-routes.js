8963d96794d50caffb4e49083cac4213c214ceb7
Inbound Now <david@inboundnow.com>
2015-05-12 13:25:24 -0700

    rename files with - for live server issue fixes

--- app/routes/core-routes.js
+++ app/routes/core-routes.js
@@ -1,5 +1,5 @@
-var React = require('react/addons');
-var ReactApp = React.createFactory(require('../components/ReactApp').ReactApp);
+var React = require('react/addons'),
+ReactApp = React.createFactory(require('../components/ReactApp'));
 
 module.exports = function(app) {
 
