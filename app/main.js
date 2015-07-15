3aea3ecabbc183df4364db14832dae761c068f9a
Jonatan Dahl <jd@spotify.com>
2015-07-15 19:27:49 +0200

    use jsx to fix deprecation warning

--- app/main.js
+++ app/main.js
@@ -5,4 +5,4 @@ var ReactApp = require('./components/ReactApp');
 
 var mountNode = document.getElementById('react-main-mount');
 
-React.render(new ReactApp({}), mountNode);
+React.render(<ReactApp />, mountNode);
