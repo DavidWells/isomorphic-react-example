0ae6cdea97aa5db66eb5e2e929c4f7fc057a7b08
Ian Webster <ianw_git@ianww.com>
2015-09-23 23:14:08 -0700

    Fix double vs single quote inconsistencies and spacing.

--- app/routes/core-routes.js
+++ app/routes/core-routes.js
@@ -5,11 +5,11 @@ module.exports = function(app) {
 
 	app.get('/', function(req, res){
 		// React.renderToString takes your component
-	    // and generates the markup
+    // and generates the markup
 		var reactHtml = React.renderToString(ReactApp({}));
-	    // Output html rendered by react
+    // Output html rendered by react
 		// console.log(myAppHtml);
-	    res.render('index.ejs', {reactOutput: reactHtml});
+    res.render('index.ejs', {reactOutput: reactHtml});
 	});
 
 };
