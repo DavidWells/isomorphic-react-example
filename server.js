a63fcd5a67643549e582dfaff8f98c0c1f43cc02
Timon Sotiropoulos <timon@seedtech.io>
2015-09-28 11:31:51 +1000

    Added Nunjucks templating to server side rendering

--- server.js
+++ server.js
@@ -1,30 +1,51 @@
-// server.js
-
+// *******************************************************
+// SERVER VARIABLES
+// *******************************************************
 var express = require('express'),
+nunjucks = require('nunjucks'),
 path = require('path'),
 app = express(),
 port = 4444,
 bodyParser = require('body-parser');
 
-// Make sure to include the JSX transpiler
+// *******************************************************
+// NODE-JSX TRANSPILER
+// *******************************************************
+// This include the node-jsx transpiler to turn your react
+// components into javascript code
 require('node-jsx').install();
 
-// Include static assets. Not advised for production
+// *******************************************************
+// STATIC DIRECTORY SETUP
+// *******************************************************
 app.use(express.static(path.join(__dirname, 'public')));
-// Set view path
-app.set('views', path.join(__dirname, 'views'));
-// set up ejs for templating. You can use whatever
-app.set('view engine', 'ejs');
 
-// Set up Routes for the application
+// *******************************************************
+// NUNJUCKS TEMPLATING SETUP
+// *******************************************************
+// NOTE: using autoescape = false stops the react html
+// elements from being escaped as strings instead of html elements
+nunjucks.configure('./views', {
+    autoescape: false,
+    express: app
+});
+
+// *******************************************************
+// APP ROUTING
+// *******************************************************
 require('./app/routes/core-routes.js')(app);
 
-//Route not found -- Set 404
+// *******************************************************
+// ERROR HANDLING
+// *******************************************************
 app.get('*', function(req, res) {
     res.json({
         'route': 'Sorry this page does not exist!'
     });
 });
 
+// *******************************************************
+// SERVER LISTENING AND STARTUP
+// *******************************************************
 app.listen(port);
 console.log('Server is Up and Running at Port : ' + port);
