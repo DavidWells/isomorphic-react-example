1b55a8578692a0fa79426b1559a1f7588ccbeb6c
yannliao <trembleliao@gmail.com>
2016-08-04 21:38:37 +0800

    remove deprecated dependencies

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,13 +1,29 @@
 var gulp       = require('gulp'),
-    browserify = require('gulp-browserify');
+    browserify = require('browserify'),
+    source     = require('vinyl-source-stream'),
+    buffer     = require('vinyl-buffer'),
+    sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('scripts', function () {
 
-    gulp.src(['app/main.js'])
-        .pipe(browserify({
-            debug: true,
-            transform: [ 'reactify' ]
-        }))
+    // gulp.src(['app/main.js'])
+    //     .pipe(browserify({
+    //         debug: true,
+    //         transform: [ 'reactify' ]
+    //     }))
+    //     .pipe(gulp.dest('./public/'));
+
+
+    var bundler = browserify({
+        entries: ['app/main.js'],
+        debug: true,
+        transform: [ "babelify" ]
+    });
+    return bundler.bundle()
+        .pipe(source('bundle.js'))
+        .pipe(buffer())
+        .pipe(sourcemaps.init({ loadMaps: true }))
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('./public/'));
 
 });
